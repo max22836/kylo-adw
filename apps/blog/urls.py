eb0052ce88526c5f5594599735e5168910aa2acc
max22836 <109144937+max22836@users.noreply.github.com>
2022-07-25 18:20:27 +0300

    urls site

---
+++ apps/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from apps.blog import views
+
+urlpatterns = [
+    path('', views.blog_category_list, name='blog_category_list'),
+    path('<int:category_id>/', views.article_list, name='article_list'),
+    path('<int:category_id>/<int:article_id>/', views.article_list, name='article_list'),
+
+]
