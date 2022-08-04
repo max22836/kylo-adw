4db40a847794103d2c3e1c1e169cae38b6091732
max22836 <109144937+max22836@users.noreply.github.com>
2022-08-04 18:23:28 +0300

    admin tricks

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from apps.blog.models import Article, BlogCategory, Tag
 
 
-admin.site.register(Article)
-admin.site.register(BlogCategory)
-admin.site.register(Tag)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'image_tag_thumbnail','created_at']
+    list_display_links = ['id', 'title', 'image_tag_thumbnail']
+
+
+@admin.register(BlogCategory)
+class BlogCategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'image_tag_thumbnail']
+    list_display_links = ['id', 'name', 'image_tag_thumbnail']
+    fields = ['name', 'image_tag_thumbnail', 'image']
+    readonly_fields = ['image_tag']
+
+    @admin.register(Tag)
+    class TagAdmin(admin.ModelAdmin):
+        list_display = ['name']
